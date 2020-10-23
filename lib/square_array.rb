d397e310e5405c5353e4b0e390c0a3b54a16f2d9
Joshua Frank <eurosewusa@gmail.com>
2020-10-23 12:36:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array( array )
   counter = 0
   result = []
   while array[ counter ] do
-    result.push( array[ counter ] )
+    result << ( array[ counter ] ** 2 )
     counter += 1
   end
 end
